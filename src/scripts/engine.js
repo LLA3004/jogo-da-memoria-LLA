512f89fc1a57c1f0539c2ddc07447ea3678f28eb
LLA3004 <lucas10lyra@gmail.com>
2024-10-22 23:11:02 +0000

    ajuste da lógica do JS e dos efeitos pós click do CSS

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -17,6 +17,33 @@ let embaralhaEmoji = emojis.sort(()=>(Math.random() >0.5 ? 2 : -1))
 for(let i=0; i<emojis.length; i++){
     let box = document.createElement("div")
     box.className ="item"
+    box.onclick = handleClick
     box.innerHTML = embaralhaEmoji[i]
     document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+    if(cartaAberta.length < 2){
+        this.classList.add("caixaAberta")
+        cartaAberta.push(this)
+
+        if(cartaAberta.length == 2){
+            setTimeout(checaCartas, 500)
+        }
+    }
+}
+
+function checaCartas(){
+    if(cartaAberta[0].innerHTML === cartaAberta[1].innerHTML){
+        cartaAberta[0].classList.add("caixaBate")
+        cartaAberta[1].classList.add("caixaBate")
+    } else{
+        cartaAberta[0].classList.remove("caixaAberta")
+        cartaAberta[1].classList.remove("caixaAberta")
+    }
+    cartaAberta = []
+    if(document.querySelectorAll(".caixaBate").length === emojis.length){
+        alert("Você Venceu" + "😁")
+
+    }
 }
